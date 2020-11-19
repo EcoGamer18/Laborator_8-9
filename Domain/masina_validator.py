3925840cdf79c623728a3ccfdf574a18a1b959a6
EcoGamer18 <gabymeow07@gmail.com>
2020-11-20 00:57:43 +0200

    update

--- Domain/masina_validator.py
+++ Domain/masina_validator.py
@@ -1,9 +1,11 @@
 class MasinaValidator:
-    def validator(self,masina):
-        erori=[]
+    def validator(self, masina):
+        erori = []
         if masina.nr_km < 0:
             erori.append("Numarul de kilometrii facuti este strict mai mare ca 0.")
         if masina.anul_fabricatiei < 0:
             erori.append("Anul de fabricatie trebuie sa fie mai mare ca 0.")
+        if masina.garantie is not "False" or masina.garantie is not "True":
+            erori.append("Garantie poate avea doar valorile \"True\" sau \"False\"")
         if len(erori) > 0:
             raise ValueError(erori)
---
+++ Service/masina_service.py
@@ -0,0 +1,44 @@
+from Domain.masina import Masina
+from Domain.masina_validator import MasinaValidator
+from Repository.file_repository import FileRepository
+
+
+class MasinaService:
+    def __init__(self, masini_repository: FileRepository, masini_validator: MasinaValidator):
+        self.__masini_repository = masini_repository
+        self.__masini_validator = masini_validator
+
+    def get_all(self):
+        return self.__masini_repository.get_all()
+
+    def adaugare(self, id_masina, model, an_achizitie, nr_km, garantie):
+        """:
+        Adaugarea unei masini la dictionarul care reprezinta repository-ul
+        """
+        masina = Masina(id_masina, model, an_achizitie, nr_km, garantie)
+        self.__masini_validator.validator(masina)
+        self.__masini_repository.adaugare(masina)
+
+    def stergere(self, id_masina):
+        """
+        Stergerea unei masini din dictionarul care reprezinta repository-ul
+        """
+        self.__masini_validator.stergere(id_masina)
+
+    def modificare(self, id_masina, model, an_achizitie, nr_km, garantie):
+        masina = self.__masini_repository.get_by_id(id_masina)
+
+        if masina is None:  # daca nu exista masina cu id-ul dat
+            raise KeyError(f"Nu exista deja o masina cu id-ul {id_masina}")
+
+        if model != "":
+            masina.model = model
+        if an_achizitie != 0:
+            masina.an_achizitie = an_achizitie
+        if nr_km != 0:
+            masina.nr_km = nr_km
+        if model != "":
+            masina.garantie = garantie
+
+        self.__masini_validator.validator(masina)
+        self.__masini_repository.modificare(masina)
